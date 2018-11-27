39feaa25c8735eb9c14978d60970da96f209dda6
abrahamemmanuel <sundayemmanuelabraham@gmail.com>
2018-11-27 23:18:22 +0100

    deleted index.html file

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
-app.get('/', (req, res) => res.render('index', { list: ['a', 'b'], title: 'Library' }));
+app.get('/', (req, res) => res.render('index', { nav: [{ link: '/books', title: 'Books' }], title: 'Library' }));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
